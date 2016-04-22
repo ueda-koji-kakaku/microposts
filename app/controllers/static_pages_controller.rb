f0e19dc7180d55063ba20bdea195a9947a0cbd6d
ueda-koji-kakaku <ueda.koji.kakaku@gmail.com>
2016-04-22 04:25:46 +0000

    add search action

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,12 @@ class StaticPagesController < ApplicationController
   def home
     if logged_in?
       @micropost = current_user.microposts.build
-      @feed_items = current_user.feed_items.includes(:user).order(created_at: :desc)
+      if params[:search]
+        @feed_items = Micropost.where("content like '%" + params[:search] + "%'").order(created_at: :desc)
+      else
+        @feed_items = current_user.feed_items.includes(:user).order(created_at: :desc)
+      end
     end
   end
+
 end
